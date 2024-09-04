8a1d96cac143c7bcfce0f766ea6fc13828d60e21
Cisneros <ecisneros7@cps.k12.il.us>
2024-09-04 14:59:33 -0500

    first commit

---
+++ week2.py
@@ -0,0 +1,10 @@
+#ask someone what day it is
+day = input("what day is it?")
+print("the day is " + day)
+#ask them for their favorite holiday
+holiday = input("what's your favorite holiday?")
+print("my favorite holiday is " + holiday)
+#print out the day and their favorite holiday in
+# a sentence
+print("Today is" + day + "and my favorite holiday is" + holiday)
+# making a change to the file
